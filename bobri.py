eccb6f4bf3892145e8d7cef7bea00781b9ca06a2
RomanSeps <96355457+RomanSeps@users.noreply.github.com>
2024-09-20 11:42:29 +0200

    Create bobri.py

---
+++ bobri.py
@@ -0,0 +1,21 @@
+import json
+import random
+x = open('bobr.json')
+y = open('nora.json')
+
+class lokalita():
+    
+    def __innit__(self, bobr, nora):
+        self.bobr = json.load(x)
+        self.nora = json.load(y)
+
+    def giveBobrNora(self, bobr, nora):
+         nora = []
+         nora.append = random(bobr)
+    
+    def __str__(self):
+        
+lokace = [lokalita()]
+
+tfICare = lokalita()
+print(tfICare)
